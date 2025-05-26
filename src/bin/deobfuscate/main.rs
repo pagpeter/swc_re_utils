acc8ceca826b0f33cf4f38322b32de7d38c839c9
Peet <gh@peet.ws>
2025-05-26 19:47:13 +0200

    feat: deobfuscate sequence_exprs

--- src/bin/deobfuscate/main.rs
+++ src/bin/deobfuscate/main.rs
@@ -1,6 +1,8 @@
 use deobfuscator::utils::logger::Logger;
 use deobfuscator::utils::swc_utils;
+use deobfuscator::transformations;
 use std::{env, fs};
+use swc_ecma_visit::VisitMutWith;
 
 extern  crate  deobfuscator;
 fn main() {
@@ -16,7 +18,10 @@ fn main() {
     let src = fs::read_to_string(filename).expect("Unable to read file");
     logger.success(format!("Read {} chars from {}", src.len(), filename).as_str());
 
-    let ast = swc_utils::parse_func_str(src);
+    let mut ast = swc_utils::parse_func_str(src);
+
+    ast.visit_mut_with(&mut  transformations::sequence_exprs::Visitor {});
+
     let out = swc_utils::generate_code(ast);
     fs::write(filename.replace(".js", ".out.js"), out).expect("Unable to write file");
 }
