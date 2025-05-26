fd82cf47e6f3f790e1f1d4da7deccb7193bb09b9
Peet <gh@peet.ws>
2025-05-26 21:22:32 +0200

    chore: kinda working constant eval

--- src/transformations/constant_evaluation.rs
+++ src/transformations/constant_evaluation.rs
@@ -1,38 +1,31 @@
 use swc_common::GLOBALS;
-use swc_common::util::take::Take;
 use swc_core::ecma::ast::Expr;
 use swc_core::ecma::visit::{VisitMut, VisitMutWith};
-use swc_ecma_ast::{Lit, Module, ModuleItem, Program, Script};
+use swc_ecma_ast::{Lit, Module, ModuleItem, Program};
 use swc_ecma_minifier::eval::Evaluator;
 use swc_ecma_minifier::{self, eval, marks};
+
 struct EvaluateVisitor {
     evaluator: Evaluator,
-    program: Program,
 }
 
 impl EvaluateVisitor {
-    pub fn new(evaluator: eval::Evaluator, program: Program) -> Self {
-        Self { evaluator, program }
+    pub fn new(evaluator: eval::Evaluator) -> Self {
+        Self { evaluator }
     }
 }
 
 impl VisitMut for EvaluateVisitor {
-    fn visit_mut_program(&mut self, node: &mut Program) {
-        node.visit_mut_children_with(self);
-        println!("[*] Running constant evaluation 2");
-        self.program = node.clone()
-    }
-
     fn visit_mut_expr(&mut self, expr: &mut Expr) {
         expr.visit_mut_children_with(self);
 
         if let Some(res) = self.evaluator.eval(expr) {
             match res {
                 eval::EvalResult::Lit(lit) => match lit {
-                    Lit::Bool(l) => *expr = Expr::Lit(swc_ecma_ast::Lit::Bool(l.to_owned())),
-                    // Lit::Str(l) => *expr = Expr::Lit(swc_ecma_ast::Lit::Str(l.to_owned())),
-                    // Lit::Null(l) => *expr = Expr::Lit(swc_ecma_ast::Lit::Null(l.to_owned())),
-                    // Lit::Num(l) => *expr = Expr::Lit(swc_ecma_ast::Lit::Num(l.to_owned())),
+                    Lit::Bool(l) => *expr = Expr::Lit(swc_ecma_ast::Lit::Bool(l)),
+                    Lit::Str(l) => *expr = Expr::Lit(swc_ecma_ast::Lit::Str(l)),
+                    Lit::Null(l) => *expr = Expr::Lit(swc_ecma_ast::Lit::Null(l)),
+                    Lit::Num(l) => *expr = Expr::Lit(swc_ecma_ast::Lit::Num(l)),
                     _ => {}
                 },
                 eval::EvalResult::Undefined => {}
@@ -40,7 +33,9 @@ impl VisitMut for EvaluateVisitor {
         }
     }
 }
+
 pub struct Visitor;
+
 impl VisitMut for Visitor {
     fn visit_mut_program(&mut self, n: &mut Program) {
         println!("[*] Running constant evaluation");
@@ -64,11 +59,9 @@ impl VisitMut for Visitor {
             };
 
             let evaluator = Evaluator::new(module, m);
-            let mut visitor = EvaluateVisitor::new(evaluator, Program::dummy());
+            let mut visitor = EvaluateVisitor::new(evaluator);
 
             n.visit_mut_with(&mut visitor);
-
-            *n = visitor.program;
         })
     }
 }
